72ca84361aee38314307b1acdde77fb6de4d51d7
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-03-12 08:59:20 +0530

    11-march

---
+++ 11-march/Landing-Page/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import MainContentArea from "./MainContentArea/MainContentArea";
+
+ export default function App() {
+  return(
+    <>
+      <Header/>
+      <MainContentArea/>
+      <Footer/>
+    </>
+  )
+}
+
+
---
+++ 11-march/Landing-Page/src/MainContentArea/MainContentArea.jsx
@@ -0,0 +1,62 @@
+import prod from "../assets/Product.png";
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaInstagram,
+    FaLinkedinIn,
+    FaPinterest,
+    FaCartPlus,
+} from "react-icons/fa";
+
+
+import "./MainContentArea.scss"
+
+
+export default function MainContentArea() {
+    
+    return (
+        <div className="single-product-main-content">
+            <div className="layout">
+                <div className="single-product-page">
+                    <div className="left">
+                        <img  src="https://cdn.shopify.com/s/files/1/0057/8938/4802/products/ion-W_2500x.png?v=1678780574"/>
+                    </div>
+                    <div className="right">
+                        <span className="name">Nirvana Ion</span>
+                        <span className="price">&#8377;2,299</span>
+                        <span className="desc">Bluetooth Wireless Earbuds with Crystal Bionic Sound powered by HiFi®️ DSP, 120 Hours Playback, Dual EQ Modes, Quad Mics with ENx™ Technology</span>
+
+                        <div className="cart-buttons">
+                            <div className="quantity-buttons">
+                                <span>-</span>
+                                <span>5</span>
+                                <span>+</span>
+                            </div>
+                            <button className="add-to-cart-button">
+                                <FaCartPlus size={20} />
+                                ADD TO CART
+                            </button>
+                        </div>
+                        <span className="divider" />
+                        <div className="info-item">
+                            <span className="text-bold">
+                                Category:
+                                <span>Headphones</span>
+                            </span>
+                            <span className="text-bold">
+                                Share:
+                                <span className="social-icons">
+                                    <FaFacebookF size={16} />
+                                    <FaTwitter size={16} />
+                                    <FaInstagram size={16} />
+                                    <FaLinkedinIn size={16} />
+                                    <FaPinterest size={16} />
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
